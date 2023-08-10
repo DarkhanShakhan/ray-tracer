f17877c5932b409cf5bde79295867f56b5906795
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2023-08-11 02:45:32 +0600

    adds viewport

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,78 @@
 use indicatif::{ProgressBar, ProgressStyle};
 mod color;
+mod ray;
 use prima::geom::Vec3;
 
 fn main() {
-    ppm_image();
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1
+    let mut image_height = (image_width as f32 / aspect_ratio) as i32;
+    if image_height < 1 {
+        image_height = 1;
+    }
+
+    // Camera
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f32 / image_height as f32);
+    let camera_center = Vec3::new(0.0, 0.0, 0.0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    let pixel_delta_u = viewport_u / image_width as f32;
+    let pixel_delta_v = viewport_v / image_height as f32;
+
+    // Calculate the location of the upper left pixel.
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    // let ppm_image(image_width, image_height);
+    let bar = ProgressBar::new(image_height as u64);
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in (0..image_height).rev() {
+        bar.inc(1);
+        for i in 0..image_width {
+            let pixel_center =
+                pixel00_loc + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let c = ray_color(ray::Ray::new(camera_center, ray_direction));
+            color::write_color(c);
+        }
+    }
 }
 
-fn ppm_image() {
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
-    let bar = ProgressBar::new(IMAGE_HEIGHT as u64);
-    let mut c: Vec3;
-    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+fn ray_color(r: ray::Ray) -> Vec3 {
+    let unit_direction = r.direction();
+    let a = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.5, 0.7, 1.0)
+}
 
-    for j in (0..IMAGE_HEIGHT).rev() {
+fn ppm_image(image_width: i32, image_height: i32) {
+    let bar = ProgressBar::new(image_height as u64);
+    let mut c: Vec3;
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in (0..image_height).rev() {
         bar.inc(1);
-        for i in 0..IMAGE_WIDTH {
+        for i in 0..image_width {
             c = Vec3::new(
-                i as f32 / (IMAGE_WIDTH - 1) as f32,
-                j as f32 / (IMAGE_HEIGHT - 1) as f32,
+                i as f32 / (image_width - 1) as f32,
+                j as f32 / (image_height - 1) as f32,
                 0.25,
             );
             color::write_color(c);
         }
     }
+    let r = ray::Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(1.0, 2.0, 1.0));
+    r.at(0.5);
+
     bar.set_style(ProgressStyle::with_template("{msg}").unwrap());
     bar.finish_with_message("done");
 }
--- src/ray/mod.rs
+++ src/ray/mod.rs
@@ -1,1 +1,21 @@
+use prima::geom::Vec3;
 
+pub struct Ray {
+    orig: Vec3,
+    dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(orig: Vec3, dir: Vec3) -> Self {
+        Ray { orig, dir }
+    }
+    pub fn at(&self, t: f32) -> Vec3 {
+        self.orig + (t * self.dir)
+    }
+    // pub fn origin(&self) -> Vec3 {
+    //     self.orig
+    // }
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+}
